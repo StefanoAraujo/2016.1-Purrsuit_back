9264b6aeafdb5291559f4fddf0619789c3e6b9a1
brunobrg <bbraganca1991@gmail.com>
2016-06-09 21:06:05 -0300

    issue#88 reconfiguring the travis and codeclimate integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
