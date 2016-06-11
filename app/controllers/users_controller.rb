fee87a95db0ff1f465da8651a7827e496ec6b033
Ramon Sales <ramon.silvasales@gmail.com>
2016-06-10 23:29:08 -0300

    fixing ionic login & create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
     begin
       if saved
         session[:user_id] = user.id
-        render json: user
+        render :nothing => true
       else
         raise "sign up failed"
       end
@@ -108,7 +108,7 @@ class UsersController < ApplicationController
       if user && user.authenticate(params[:password])
         puts "login successfull!"
         puts user
-        render json: user
+        render :nothing => true
       else
         raise "login failed!"
       end
