4d6df1017ba05175f75bb02d4d169e4953d851a0
Thiagotrfm <t_moreira@outlook.com>
2016-06-14 01:46:42 -0300

    Adding edit test
    Signed-off-by: Thiagotrfm <t_moreira@outlook.com>
    Signed-off-by: Tulio Coelho <tulioopc@gmail.com>

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -1,27 +1,40 @@
 require 'spec_helper'
 
 describe AchievementsController do
-	
-	#Missing #edit #all #show
-	
+
 	before :each do
 		@achievement = create(:achievement)
 	end
-	
+
+	describe "GET #all" do
+		it "returns all achievements json" do
+			achv_all = Achievement.all
+			achv_json = achv_all.map{|achv| AchievementSerializer.new(achv)}.to_json
+			get :all, :format => 'json'
+			expect(response.body).to match(achv_json)
+		end
+	end
+
 	describe 'GET #show' do
 		it "assigns the requested achievement to @achievement" do
 			get 'show', :id => @achievement
 			expect(assigns(:achievement)).to eq @achievement
 		end
+		it "does not find the requested achievement" do
+			get 'delete', :id => @achievement
+			expect{
+				get 'show', :id => @achievement
+			}.to raise_error(RuntimeError, "ERROR")
+		end
 	end
-	
+
 	describe 'GET #new' do
 		it "assigns a new Achievement to @Achievement" do
 			get :new
 			expect(assigns(:achievement)).to be_a_new(Achievement)
 		end
 	end
-	
+
 	describe 'POST #create' do
 		context "with valids attributes" do
 			it "saves the new achievement in the database" do
@@ -30,17 +43,8 @@ describe AchievementsController do
 				}.to change(Achievement, :count).by(1)
 			end
 		end
-=begin
-		context "without valids attributes" do
-			it "does not save the achievement in the database" do
-				expect{
-					post 'create', achievement: attributes_for(:achievement, name: nil)
-				}.to_not change(Achievement, :count)
-			end
-		end
-=end
 	end
-	
+
 	describe 'DELETE #delete' do
 		it "delete an achievement with a given id" do
 			achievement_example = create(:achievement)
@@ -50,13 +54,23 @@ describe AchievementsController do
 		end
 	end
 	
+	describe 'GET #edit' do
+		it "should return sucess" do
+			post :edit, id: @achievement
+			expect(response).to have_http_status(200)
+		end
+	end
+
 	describe 'PATCH #update' do
 		it "changes an achievement attibute" do
-			achievement = create(:achievement)
-			old_name = achievement.name
-			patch 'update', id: achievement, achievement: attributes_for(:achievement, name: "A different name")
-			new_name = achievement.name
+			old_name = @achievement.name
+			patch 'update', id: @achievement, achievement: attributes_for(:achievement, name: "A different name")
+			new_name = @achievement.name
 			expect(new_name!=old_name)
+		end
+		it "has an invalid attribute value" do
+			expect(
+				patch 'update', id: @achievement, achievement: attributes_for(:achievement, name: nil)).to render_template(:edit)
 		end
 	end
 end
--- spec/controllers/quests_controller_spec.rb
+++ spec/controllers/quests_controller_spec.rb
@@ -3,15 +3,30 @@ require 'spec_helper'
 describe QuestsController do
 
 	#Missing #edit #all #show
-	
+
 	before :each do
 		@quest = create(:quest)
+	end
+
+	describe "GET #all" do
+		it "returns all quests json" do
+			quest_all = Quest.all
+			quest_json = quest_all.map{|quest| QuestSerializer.new(quest)}.to_json
+			get :all, :format => 'json'
+			expect(response.body).to match(quest_json)
+		end
 	end
 
 	describe 'GET #show' do
 		it "assigns the requested quest to @quest" do
 			get 'show', :id => @quest
 			expect(assigns(:quest)).to eq @quest
+		end
+		it "does not find the requested quest" do
+			get 'delete', :id => @quest
+			expect{
+				get 'show', :id => @quest
+			}.to raise_error(RuntimeError, "ERROR")
 		end
 	end
 
@@ -26,7 +41,7 @@ describe QuestsController do
 		context "with valids attributes" do
 			it "saves the new quest in the database" do
 				expect{
-					post 'create', quest: attributes_for(:quest)	
+					post 'create', quest: attributes_for(:quest)
 				}.to change(Quest, :count).by(1)
 			end
 		end
@@ -38,7 +53,7 @@ describe QuestsController do
 			end
 		end
 	end
-	
+
 	describe 'DELETE #delete' do
 		it "delete a quest with a given id" do
 			quest_example = create(:quest)
@@ -47,14 +62,25 @@ describe QuestsController do
 			}.to change(Quest, :count).by(-1)
 		end
 	end
+	
+	describe 'GET #edit' do
+		it "should return sucess" do
+			post :edit, id: @quest
+			expect(response).to have_http_status(200)
+		end
+	end
 
 	describe 'PATCH #update' do
 		it "changes a quest attribute" do
-			quest = create(:quest)
-			old_name = quest.name
-			patch 'update', id: quest, quest: attributes_for(:quest, name: "A different name")
-			new_name = quest.name
+			old_name = @quest.name
+			patch 'update', id: @quest, quest: attributes_for(:quest, name: "A different name")
+			new_name = @quest.name
 			expect(new_name!=old_name)
+		end
+		it "has an invalid attribute value" do
+			expect(
+				patch 'update', id: @quest, quest: attributes_for(:quest, name: nil)
+			).to render_template(:edit)
 		end
 	end
 end
