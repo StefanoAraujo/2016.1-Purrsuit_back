8a10e0c2e337941bc711a4c7e9f2b9ea3a403910
Tulio Coelho <tulioopc@gmail.com>
2016-06-17 12:52:05 -0300

    Changing http request to post

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'users/:id/edit' => 'users#edit', format: :json, as: :users_edit
   post 'users/:id/follow_deputy' => 'users#follow_deputy', format: :json
   post 'users/:id/unfollow_deputy' => 'users#unfollow_deputy', format: :json
-  put 'users/:id/update' => 'users#update', format: :json, as: :users_update
+  post 'users/:id/update' => 'users#update', format: :json, as: :users_update
   delete 'users/delete/:id' => 'users#delete', as: :users_delete
 
   get 'deputies/all' => 'deputies#all', format: :json
@@ -23,14 +23,14 @@ Rails.application.routes.draw do
   post 'achievements/create' => 'achievements#create', as: :achievements
   get 'achievements/:id' => 'achievements#show', format: :json
   get 'achievements/:id/edit' => 'achievements#edit', format: :json, as: :achievements_edit
-  put 'achievements/:id/update' => 'achievements#update',as: :achievements_update
+  post 'achievements/:id/update' => 'achievements#update',as: :achievements_update
 	delete 'achievements/:id' => 'achievements#delete', as: :achievements_delete
 
   get 'quests/all' => 'quests#all', format: :json, as: :quests_all
 	get 'quests/new' => 'quests#new', format: :json, as: :quests_new
 	post 'quests/create' => 'quests#create', as: :quests
 	get 'quests/:id' => 'quests#show', format: :json
-	put 'quests/:id/update' => 'quests#update', format: :json, as: :quests_update
+	post 'quests/:id/update' => 'quests#update', format: :json, as: :quests_update
 	get 'quests/:id/edit' => 'quests#edit', format: :json, as: :quests_edit
 	delete 'quests/:id' => 'quests#delete', as: :quests_delete
 
